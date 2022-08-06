bbda50e2b54401c4540ac6252075a6d3e94afab4
ravinduf <ravinduf@rocketmail.com>
2022-08-06 15:20:48 +0530

    created forward_to_target_url function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import secrets
+from urllib.request import Request
 
 import validators
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
+from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from . import schemas, models
@@ -23,6 +25,10 @@ def get_db():
 
 def raise_bad_request(message):
     raise HTTPException(status_code=400, detail=message)
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
 
 @app.get("/")
 def read_root():
@@ -48,3 +54,21 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
     db_url.admin_url = secret_key
 
     return db_url
+
+@app.get("/{url_key}")
+def forward_to_target_url(
+        url_key: str,
+        request: Request,
+        db: Session = Depends(get_db)
+    ):
+
+    db_url = (
+        db.query(models.URL)
+            .filter(models.URL.key == url_key, models.URL.is_active)
+            .first()
+    )
+
+    if db_url:
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
