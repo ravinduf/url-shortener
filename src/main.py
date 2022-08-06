d5157adb693b1d5fce2dd781538b0d53d01e7138
ravinduf <ravinduf@rocketmail.com>
2022-08-06 18:08:49 +0530

    added get_admin_info func and updated

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-from urllib.request import Request
+# from urllib.request import Request
 
 import validators
 from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 
 from . import schemas, models, crud
 from .database import SessionLocal, engine
+from .config import get_settings
 
 app = FastAPI()
 
@@ -29,6 +31,15 @@ def raise_not_found(request):
     message = f"URL '{request.url}' doesn't exist"
     raise HTTPException(status_code=404, detail=message)
 
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
 @app.get("/")
 def read_root():
     return "Welcome to the URL shortener API"
@@ -39,10 +50,8 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
         raise_bad_request(message="Your provided URL is not valid")
     
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
 
-    return db_url
+    return get_admin_info(db_url)
 
 @app.get("/{url_key}")
 def forward_to_target_url(
@@ -53,5 +62,20 @@ def forward_to_target_url(
 
     if db_url := crud.get_db_url_by_key(db = db, url_key = url_key):
         return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name=" administration info",
+    response_model=schemas.URLInfo,
+)
+def get_url_info(
+    secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key = secret_key):
+        return get_admin_info(db_url)
     else:
         raise_not_found(request)
